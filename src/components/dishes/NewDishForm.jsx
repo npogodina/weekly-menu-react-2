e3879e54b159dd68a2f01b59c646c87ca7272e00
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-04 16:51:41 -0700

    Added state to NewDishForm

--- src/components/dishes/NewDishForm.jsx
+++ src/components/dishes/NewDishForm.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Table } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 import PropTypes from "prop-types";
 
 const NewDishForm = (props) => {
+  const [formFields, setFormFields] = useState({
+    name: "",
+    breakfast: false,
+    lunch: false,
+    dinner: false,
+    other: false,
+    servings: 1,
+    directions: [],
+    ingredients: [],
+  });
+
   return <div>New!</div>;
 };
 
