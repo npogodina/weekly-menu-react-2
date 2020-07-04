9d04a5686743fd852c7648f61d7b9482155a35d3
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-04 16:25:06 -0700

    Added styling to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+
+import { Input, Menu, Segment } from "semantic-ui-react";
+
 import PropTypes from "prop-types";
 
 const Navbar = () => {
-  return <div></div>;
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
+  return (
+    <div>
+      <Menu pointing>
+        <Menu.Item
+          name="home"
+          active={activeItem === "home"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="messages"
+          active={activeItem === "messages"}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="friends"
+          active={activeItem === "friends"}
+          onClick={handleItemClick}
+        />
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Input icon="search" placeholder="Search..." />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </div>
+  );
 };
 
 export default Navbar;
