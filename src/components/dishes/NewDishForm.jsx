6641a1ac316c1ba1f573f8a5a79760b3e3a7f49d
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-04 18:51:11 -0700

    Added servings input field

--- src/components/dishes/NewDishForm.jsx
+++ src/components/dishes/NewDishForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-import { Container, Form, Button, Checkbox } from "semantic-ui-react";
+import { Container, Form, Button, Checkbox, Select } from "semantic-ui-react";
 
 import PropTypes from "prop-types";
 
@@ -16,6 +18,17 @@ const NewDishForm = (props) => {
     ingredients: [],
   });
 
+  const options = [
+    { key: "1", text: "1", value: "1" },
+    { key: "2", text: "2", value: "2" },
+    { key: "3", text: "3", value: "3" },
+    { key: "4", text: "4", value: "4" },
+    { key: "5", text: "5", value: "5" },
+    { key: "6", text: "6", value: "6" },
+    { key: "7", text: "7", value: "7" },
+    { key: "8", text: "8", value: "8" },
+  ];
+
   const onInputChange = (event) => {
     const newFormFields = {
       ...formFields,
@@ -24,9 +37,31 @@ const NewDishForm = (props) => {
     setFormFields(newFormFields);
   };
 
+  const onSelectChange = (event, result) => {
+    const newFormFields = {
+      ...formFields,
+    };
+    const { name, value } = result;
+
+    newFormFields[name] = value;
+    setFormFields(newFormFields);
+  };
+
+  let history = useHistory();
   const onFormSubmit = (event) => {
     event.preventDefault();
-    //history.push(`/dishes/`)
+    axios
+      .post(process.env.REACT_APP_API_DISHES_INDEX, formFields)
+      .then((response) => {
+        console.log("Post request sent!");
+        history.push(`/dishes/`);
+      })
+      .catch((error) => {
+        // What should we do when we know the post request failed?
+        // setErrorMessage(error.message);
+      });
+
+    //
   };
 
   return (
@@ -42,6 +77,16 @@ const NewDishForm = (props) => {
             value={formFields.name}
           />
         </Form.Field>
+        <Form.Select
+          control={Select}
+          label="How many servings?"
+          placeholder="Servings"
+          name="servings"
+          selection
+          options={options}
+          onChange={onSelectChange}
+          value={formFields.servings}
+        />
         <Button type="submit">Submit</Button>
       </Form>
     </Container>
