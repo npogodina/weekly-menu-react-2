f51df8d2a3b671b9d07f768a7b0986dd30e5d0ca
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-06 06:45:30 -0700

    Added ingredients input

--- src/components/dishes/NewDishForm.jsx
+++ src/components/dishes/NewDishForm.jsx
@@ -30,6 +30,7 @@ const NewDishForm = (props) => {
   });
 
   const [directions, setDirections] = useState([""]);
+  const [ingredients, setIngredients] = useState([{}]);
 
   const options = [
     { key: "1", text: "1", value: "1" },
@@ -80,6 +81,18 @@ const NewDishForm = (props) => {
     setDirections(values);
   };
 
+  const onIngredientsChange = (i, event) => {
+    const values = [...ingredients];
+    values[i][event.target.name] = [event.target.value][0];
+    setIngredients(values);
+  };
+
+  const addIngredient = () => {
+    const values = [...ingredients];
+    values.push({});
+    setIngredients(values);
+  };
+
   let history = useHistory();
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -94,18 +107,24 @@ const NewDishForm = (props) => {
       }
     });
 
+    ingredients.forEach((ingredient) => {
+      if (ingredient.name !== "") {
+        newFormFields["ingredients"].push(ingredient);
+      }
+    });
+
     console.log(newFormFields);
 
-    axios
-      .post(process.env.REACT_APP_API_DISHES_INDEX, formFields)
-      .then((response) => {
-        console.log("Post request sent!");
-        history.push(`/dishes/`);
-      })
-      .catch((error) => {
-        // What should we do when we know the post request failed?
-        // setErrorMessage(error.message);
-      });
+    // axios
+    //   .post(process.env.REACT_APP_API_DISHES_INDEX, formFields)
+    //   .then((response) => {
+    //     console.log("Post request sent!");
+    //     history.push(`/dishes/`);
+    //   })
+    //   .catch((error) => {
+    //     // What should we do when we know the post request failed?
+    //     // setErrorMessage(error.message);
+    //   });
   };
 
   return (
@@ -179,6 +198,40 @@ const NewDishForm = (props) => {
                 <input
                   placeholder={placeholder}
                   onChange={(e) => onDirectionsChange(idx, e)}
+                />
+              </Form.Field>
+            </Form.Group>
+          );
+        })}
+
+        <h2>Ingredients:</h2>
+        {ingredients.map((ingredients, idx) => {
+          let placeholder = "Ingredient " + (idx + 1);
+          return (
+            <Form.Group widths="equal">
+              <Icon bordered name="plus" size="large" onClick={addIngredient} />
+
+              <Form.Field>
+                <input
+                  name="amount"
+                  placeholder="1"
+                  onChange={(e) => onIngredientsChange(idx, e)}
+                />
+              </Form.Field>
+
+              <Form.Field>
+                <input
+                  name="measurement"
+                  placeholder="lbs"
+                  onChange={(e) => onIngredientsChange(idx, e)}
+                />
+              </Form.Field>
+
+              <Form.Field>
+                <input
+                  name="name"
+                  placeholder="cranberries"
+                  onChange={(e) => onIngredientsChange(idx, e)}
                 />
               </Form.Field>
             </Form.Group>
