4242a94cda635af0a16fa8c3543265d701e26d5c
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-06 19:22:57 -0700

    Added context and styling

--- src/components/menus/NewMenuPage.jsx
+++ src/components/menus/NewMenuPage.jsx
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 // import { useAuth0 } from "@auth0/auth0-react";
-import { Form } from "semantic-ui-react";
+import { Container, Form, Button } from "semantic-ui-react";
 
 const NewMenuPage = (props) => {
   // const { user, isAuthenticated } = useAuth0();
@@ -14,9 +14,15 @@ const NewMenuPage = (props) => {
   };
 
   return (
-    <Form.Field>
-      <DatePicker selected={startDate} onChange={onDateChange} />
-    </Form.Field>
+    <Container className="cont">
+      <h2>Please, select the start date for the next week's menu.</h2>
+      <Form onSubmit="onFormSubmit">
+        <DatePicker selected={startDate} onChange={onDateChange} />
+        <div className="cont">
+          <Button type="submit">Generate menu!</Button>
+        </div>
+      </Form>
+    </Container>
   );
 };
 
