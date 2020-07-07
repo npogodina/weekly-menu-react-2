7e36fdd3b0bfa908234d2fcde1f3f2636bb969f0
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-06 19:15:08 -0700

    Added datepicker to NewMenuPage

--- src/components/menus/NewMenuPage.jsx
+++ src/components/menus/NewMenuPage.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 // import { useAuth0 } from "@auth0/auth0-react";
-import { Card, Container, Grid, Image } from "semantic-ui-react";
+import { Form } from "semantic-ui-react";
 
 const NewMenuPage = (props) => {
   // const { user, isAuthenticated } = useAuth0();
+  const [startDate, setStartDate] = useState(new Date());
 
-  return <h1>Hi!</h1>;
+  const onDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  return (
+    <Form.Field>
+      <DatePicker selected={startDate} onChange={onDateChange} />
+    </Form.Field>
+  );
 };
 
 export default NewMenuPage;
