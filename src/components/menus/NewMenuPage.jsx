e55adc9fc4e695cc0fc7a0c351451e4b22f4af1a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-19 13:56:52 -0700

    Cleaned up NewMenuPage

--- src/components/menus/NewMenuPage.jsx
+++ src/components/menus/NewMenuPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -35,7 +35,6 @@ const NewMenuPage = (props) => {
         const message = `Successfully generated new menu`;
         const type = "success";
         props.setMessage(message, type);
-        // setMenu(response.data.menu);
       })
       .catch((error) => {
         // What should we do when we know the post request failed?
@@ -43,77 +42,17 @@ const NewMenuPage = (props) => {
       });
   };
 
-  const dates = [startDate.toISOString()];
-  for (let i = 1; i < 7; i++) {
-    let day = new Date(startDate);
-    day.setDate(day.getDate() + i);
-    dates.push(day.toISOString());
-  }
-
-  console.log(dates);
-
-  let menuLines = null;
-  let menuLinesToRender = null;
-
-  if (menu) {
-    menuLines = (dates, menu) => {
-      return dates.map((day) => {
-        return (
-          <Table.Row>
-            <Table.Cell>{dateformat(day, "m/d ddd")}</Table.Cell>
-            <Table.Cell>{menu[day]["breakfast"]}</Table.Cell>
-            <Table.Cell>{menu[day]["lunch"]}</Table.Cell>
-            <Table.Cell>{menu[day]["dinner"]}</Table.Cell>
-          </Table.Row>
-        );
-      });
-    };
-
-    menuLinesToRender = <Table.Body>{menuLines(dates, menu)}</Table.Body>;
-  }
-
   return (
     <Container className="cont">
       <Card fluid>
         <Card.Content>
-          {!menu && (
-            <div>
-              <h2>Please, select the start date for the next week's menu.</h2>
-              <Form onSubmit={onFormSubmit}>
-                <Form.Field>
-                  <DatePicker selected={startDate} onChange={onDateChange} />
-                </Form.Field>
-                <Button type="submit">Generate Menu</Button>
-              </Form>
-            </div>
-          )}
-
-          {menu && (
-            <div>
-              <h2>Here's what we suggest!</h2>
-              <Table definition celled>
-                <Table.Header>
-                  <Table.Row>
-                    <Table.HeaderCell width={2} />
-                    <Table.HeaderCell width={4}>Breakfast</Table.HeaderCell>
-                    <Table.HeaderCell width={4}>Lunch</Table.HeaderCell>
-                    <Table.HeaderCell width={4}>Dinner</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
-                {menuLinesToRender}
-              </Table>
-              <h2>Happy?</h2>
-              <Button.Group>
-                <Button color="green">Yes!</Button>
-                <Button.Or />
-                <Button color="yellow">Edit</Button>
-                <Button.Or />
-                <Button color="orange">Redo</Button>
-                <Button.Or />
-                <Button color="red">Cancel</Button>
-              </Button.Group>
-            </div>
-          )}
+          <h2>Please, select the start date for the next week's menu.</h2>
+          <Form onSubmit={onFormSubmit}>
+            <Form.Field>
+              <DatePicker selected={startDate} onChange={onDateChange} />
+            </Form.Field>
+            <Button type="submit">Generate Menu</Button>
+          </Form>
         </Card.Content>
       </Card>
     </Container>
