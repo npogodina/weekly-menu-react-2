dde32dd98b7f17d778db8c0972c349e0ecd3577d
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-19 15:13:48 -0700

    Added blog post to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
 
-import { Container, Card, Advertisement } from "semantic-ui-react";
+import { Item, Card, Container } from "semantic-ui-react";
 import "./Home.css";
+import Nataliya from "../img/nataliya.jpg";
 
 import PropTypes from "prop-types";
 
@@ -26,21 +27,54 @@ const Home = (props) => {
           <p>Enjoy!</p>
         </div>
       </div>
-      {/* <Card fluid id="home-card">
-        <Card fluid id="home-card-inner">
-          <Card.Content>
-            <p>Hello and Welcome!</p>
-            <p>How it works:</p>
-            <ol>
-              <li>Log in and add your dishes</li>
-              <li>Let us make a menu out of them</li>
-              <li>Change what you don't like</li>
-              <li>Tadam! Here's your menu and your grocery list!</li>
-            </ol>
-            <p>What are you waiting for?</p>
-          </Card.Content>
-        </Card>
-      </Card> */}
+
+      <Container className="cont mt-35">
+        {/* <Card fluid>
+          <Card.Content> */}
+        <Item.Group className="blog-post">
+          <Item>
+            <Item.Image
+              size="small"
+              src={Nataliya}
+              circular
+              className="avatar"
+            />
+
+            <Item.Content>
+              <Item.Header>Nataliya</Item.Header>
+              <Item.Description>
+                <p>Thank you for visiting Weekly Menu!</p>
+                <p>This is my Capstone project for Ada Developers Academy.</p>
+                <p>
+                  If you like the app and want to use it, please go ahead! I
+                  plan to keep working on the project and add more features, and
+                  I have no intention to turn it off. You are also very welcome
+                  to reach out with any feedback using the contact information
+                  on the bottom of the page.
+                </p>
+                <p>
+                  If you're interested in technical details, please check my
+                  Github. The back end was made using AWS serverless stack: AWS
+                  Lambda (JS), API Gateway and DynamoDB. The front end is in
+                  React.
+                </p>
+                <p>
+                  Ada Developers Academy is a non-profit tuition-free intensive
+                  coding school for women and gender diverse people. It combines
+                  six months of classroom training with a five month internship
+                  at Seattle tech companies. As a culmination of the classroom
+                  learning experience, students create a Capstone project where
+                  they are encouraged to learn and implement new technologies.
+                  For more information about Ada please visit the school's
+                  homepage.
+                </p>
+              </Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+        {/* </Card.Content>
+        </Card> */}
+      </Container>
     </div>
   );
 };
