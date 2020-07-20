a143a191075efc9f69f9feb96f5329df8001013a
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-19 20:12:20 -0700

    Added links to the welcome message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -54,9 +54,15 @@ const Home = (props) => {
                 </p>
                 <p>
                   If you're interested in technical details, please check my
-                  Github. The back end was made using AWS serverless stack: AWS
-                  Lambda (JS), API Gateway and DynamoDB. The front end is in
-                  React.
+                  <span> </span>
+                  <a
+                    target="_blank"
+                    href="https://github.com/npogodina/weekly-menu-sls"
+                  >
+                    Github
+                  </a>
+                  . The back end was made using AWS serverless stack: AWS Lambda
+                  (JS), API Gateway and DynamoDB. The front end is in React.
                 </p>
                 <p>
                   Ada Developers Academy is a non-profit tuition-free intensive
@@ -65,8 +71,11 @@ const Home = (props) => {
                   at Seattle tech companies. As a culmination of the classroom
                   learning experience, students create a Capstone project where
                   they are encouraged to learn and implement new technologies.
-                  For more information about Ada please visit the school's
-                  homepage.
+                  For more information about Ada please visit <span> </span>
+                  <a target="_blank" href="https://adadevelopersacademy.org/">
+                    the school's homepage
+                  </a>
+                  .
                 </p>
               </Item.Description>
             </Item.Content>
