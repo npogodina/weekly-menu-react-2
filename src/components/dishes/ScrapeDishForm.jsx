26999add0d8e72cd4ba770a0242e90b62ee7eda7
Nataliya Pogodina <n.pogodina@gmail.com>
2020-07-28 14:24:57 -0700

    Added onInputChange and onFormSubmit to ScrapeDishForm

--- src/components/dishes/ScrapeDishForm.jsx
+++ src/components/dishes/ScrapeDishForm.jsx
@@ -10,37 +10,43 @@ const ScrapeDishPage = (props) => {
   const [sending, setSending] = useState(false);
   const { user, isAuthenticated } = useAuth0();
 
+  const [url, setUrl] = useState("");
+
+  const onInputChange = (event) => {
+    setUrl(event.target.value);
+  };
+
   let history = useHistory();
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     console.log("Executing onFormSubmit");
-    // console.log(process.env.REACT_APP_API_MENUS_INDEX);
-    // setSending(true);
-    // axios
-    //   .post(process.env.REACT_APP_API_MENUS_INDEX, {
-    //     userId: user.sub,
-    //     startDate: startDate,
-    //     familySize: familySize,
-    //   })
-    //   .then((response) => {
-    //     setSending(false);
-    //     console.log("Post request sent!");
-    //     console.log(response);
-    //     history.push(`/menus/${response.data.menuId}`);
-    //     const message = `Successfully generated new menu`;
-    //     const type = "success";
-    //     props.setMessage(message, type);
-    //   })
-    //   .catch((error) => {
-    //     console.log(error.response.data);
-    //     setSending(false);
-    //     const message = error.response.data;
-    //     const type = "error";
-    //     props.setMessage(message, type);
-    //     // What should we do when we know the post request failed?
-    //     // setErrorMessage(error.message);
-    //   });
+    setSending(true);
+    axios
+      .post(
+        "https://am54xet7t0.execute-api.us-west-2.amazonaws.com/dev/scraper",
+        {
+          dishUrl: url,
+        }
+      )
+      .then((response) => {
+        setSending(false);
+        console.log("Post request sent!");
+        console.log(response);
+        // history.push(`/menus/${response.data.menuId}`);
+        const message = `Successfully scraped a dish`;
+        const type = "success";
+        props.setMessage(message, type);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        setSending(false);
+        const message = error.response.data;
+        const type = "error";
+        props.setMessage(message, type);
+        // What should we do when we know the post request failed?
+        // setErrorMessage(error.message);
+      });
   };
 
   return (
@@ -48,13 +54,23 @@ const ScrapeDishPage = (props) => {
       {sending && <Loading />}
       <Card fluid>
         <Card.Content>
-          <h1>Making a new menu</h1>
-          <h3>Please, select the start date for the next week's menu.</h3>
+          <h1>Adding a dish from the Internet</h1>
           <Form onSubmit={onFormSubmit}>
-            <Form.Field></Form.Field>
+            <Form.Field>
+              <label>
+                <h3>Please, provide a URL to your dish.</h3>
+              </label>
+              <input
+                placeholder="Example: https://www.allrecipes.com/recipe/212706/caramel-apple-cranberry-pie/"
+                name="url"
+                onChange={onInputChange}
+                value={url}
+                className="input-border"
+              />
+            </Form.Field>
 
             <Button type="submit" color="green">
-              Generate Menu
+              Get Dish Info
             </Button>
           </Form>
         </Card.Content>
